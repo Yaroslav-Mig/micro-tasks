f383b9f95a0da5752025fbf9525f3a30c95d8467
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2022-04-05 12:31:49 +0300

    feat: make new components with map methods, add filter using by buttons, make counter, rerender with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './01/Header';
+import { Body } from './01/Body';
+import { Footer } from './01/Footer';
+import { Button } from './01/Button';
+import { UseState } from './01/UseState';
 
 function App() {
+
+	const BtnFunc_1 = (subcriber: string, age: number) => {
+		console.log(`I\`m ${subcriber} ${age}`);
+
+	}
+	const BtnFunc_2 = (subcriber: string, age: number) => {
+		console.log(`I\`m ${subcriber} ${age}`);
+	}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header title='Hello world' />
+      <Body title='Body' />
+      <Footer title='Footer' />
+			<Button name={'Btn1'} callBack={() => BtnFunc_1('Yar', 21)}/>
+			<Button name={'Btn2'} callBack={() => BtnFunc_2('Alex', 31)} />
+			<UseState/>
     </div>
   );
 }
